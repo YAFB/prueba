6c9598d7e95c700ab7d1961fad0857f14ac8f782
SaulXibille <49504706+SaulXibille@users.noreply.github.com>
2019-04-11 11:28:04 -0600

    Commit nose que

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -69,14 +69,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <div id="container">
 
-
-
-
 	<h1>FridaGm</h1>
 
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>Nos se que ponerle</p>
 
 		<p>If you would like to edit this page you'll find it located at:</p>
 		<code>application/views/welcome_message.php</code>
